904b6f3bd5bf2c7d0fd7eec2be08ecde47b391ef
Martin Bertko <xbertko1@fi.muni.cz>
2024-10-13 23:35:04 +0200

    Revert "type error"
    This reverts commit a8bb2630eae99acac9555c6f102a6ff0f4fe38c7.

--- src/menu.ts
+++ src/menu.ts
@@ -1,6 +1,6 @@
 import settings_icon from "./img/settings.svg";
 
-export function initMenu(container: HTMLElement, selected: number) {
+export function initMenu(container: HTMLElement, selected: string) {
   const icon: HTMLImageElement = container.querySelector(".menu-icon")!;
   icon.src = settings_icon;
   const selection = container.getElementsByClassName(selected);
